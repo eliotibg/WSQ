aa94dae489fb5161a9e38f68a1e7e012355c184f
eliotibg <eliotibg1@gmail.com>
2015-03-02 11:59:11 -0600

    WSQ9
    bien

--- WSQ09.py
+++ WSQ09.py
@@ -1,7 +1,10 @@
 n=1
 e=1
-v=int(input("De que bvalor quieres saber el exponente: "))
-while(n<=v):
-	e=e*n
-	n=n+1
-print("El expoente de este numero es: ",e)
+p=str(input("Quieres conocer el factorial de algun numero ? "))
+while(p=='si' or p=='SI' or p=='Si'):
+	v=int(input("De que valor quieres saber el exponente: "))
+	while(n<=v):
+		e=e*n
+		n=n+1
+	print("El expoente de este numero es: ",e)
+	p=str(input("Quieres conocer el factorial de algun numero ? "))
